22139fe468f8e75fd9cfcde6b82c63bd3a67d9ca
tripleh1701-dev <tripleh1701@gmail.com>
2026-01-03 21:55:48 +0530

    Update outputs.tf

--- admin-portal-iac/modules/platform-bootstrap/outputs.tf
+++ admin-portal-iac/modules/platform-bootstrap/outputs.tf
@@ -153,9 +153,9 @@ output "default_enterprise_config" {
     enterprise_id   = random_uuid.global_enterprise_id.result
     enterprise_name = "Global"
     product_id      = random_uuid.platform_product_id.result
-    product_name    = "Platform"
+    product_name    = "All"
     service_id      = random_uuid.all_services_service_id.result
-    service_name    = "All Services"
+    service_name    = "All"
     linkage_id      = random_uuid.enterprise_product_service_linkage_id.result
   }
 }
